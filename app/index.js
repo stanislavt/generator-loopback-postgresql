edc1e252f897600204c24a6e39ea2eda4c83d609
Stanislav Tarasenko <stanislav.tarasenko.da14@gmail.com>
2016-11-09 12:03:33 +0200

    feat(template:env) Added .env

--- app/index.js
+++ app/index.js
@@ -5,6 +5,10 @@ let generators = require('yeoman-generator');
 module.exports = generators.Base.extend({
   writing: function () {
     this.directory('./', './');
+    this.fs.copyTpl(
+      this.templatePath('.env.example'),
+      this.destinationPath('.env')
+    );
   },
   install: function() {
     this.spawnCommand('npm', ['install']);
